83634abbf47fc89572bb82f46b43a4ae28f2abcd
Esteban <nunezesteban750@gmail.com>
2025-12-30 00:47:57 -0300

    cambio de puerto para produccion

--- backend/shared/utils/email.service.js
+++ backend/shared/utils/email.service.js
@@ -4,8 +4,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // true para 465, false para otros puertos
+    port: 587,
+    secure: false, // true para 465, false para otros puertos (587, 25)
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
