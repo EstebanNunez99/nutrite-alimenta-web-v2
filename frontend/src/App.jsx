734d60db721cadf45b630a6129e776ae2f034800
Esteban <nunezesteban750@gmail.com>
2026-03-15 15:45:30 -0300

    fix: scroll automático hacia arriba

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,12 @@ import CartProvider from './context/CartProvider.jsx';
 
 // El componente de Layout
 import MainLayout from './components/layout/MainLayout.jsx';
+import ScrollToTop from './components/routing/ScrollToTop.jsx';
 
 function App() {
     return (
         <Router>
+            <ScrollToTop />
             <AuthProvider>
                 <CartProvider>
                     <MainLayout />
